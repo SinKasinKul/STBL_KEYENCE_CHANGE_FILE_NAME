22f61eb910a9a1c33f74e821ec6ac5c2a770755c
Kasin Kuljiang <77579313+SinKasinKul@users.noreply.github.com>
2022-12-08 16:01:18 +0700

    Add project files.

---
+++ STBL_KEYENCE_CHANGE_FILE_NAME/MainForm.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace STBL_KEYENCE_CHANGE_FILE_NAME
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.tbBarcode = new System.Windows.Forms.TextBox();
+            this.lbBarcode = new System.Windows.Forms.Label();
+            this.rTBResult = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timerConnectSR1000 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // tbBarcode
+            // 
+            this.tbBarcode.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbBarcode.Location = new System.Drawing.Point(134, 12);
+            this.tbBarcode.Name = "tbBarcode";
+            this.tbBarcode.Size = new System.Drawing.Size(433, 31);
+            this.tbBarcode.TabIndex = 0;
+            // 
+            // lbBarcode
+            // 
+            this.lbBarcode.AutoSize = true;
+            this.lbBarcode.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbBarcode.Location = new System.Drawing.Point(12, 15);
+            this.lbBarcode.Name = "lbBarcode";
+            this.lbBarcode.Size = new System.Drawing.Size(116, 25);
+            this.lbBarcode.TabIndex = 1;
+            this.lbBarcode.Text = "Bracode :: ";
+            // 
+            // rTBResult
+            // 
+            this.rTBResult.Location = new System.Drawing.Point(12, 49);
+            this.rTBResult.Name = "rTBResult";
+            this.rTBResult.Size = new System.Drawing.Size(555, 147);
+            this.rTBResult.TabIndex = 2;
+            this.rTBResult.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(247, 202);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Scan";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 325);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.rTBResult);
+            this.Controls.Add(this.lbBarcode);
+            this.Controls.Add(this.tbBarcode);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "STBL Update File to serial";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbBarcode;
+        private System.Windows.Forms.Label lbBarcode;
+        private System.Windows.Forms.RichTextBox rTBResult;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timerConnectSR1000;
+    }
+}
+
---
+++ STBL_KEYENCE_CHANGE_FILE_NAME/MainForm.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Diagnostics;
+using System.Threading;
+
+namespace STBL_KEYENCE_CHANGE_FILE_NAME
+{
+    public partial class MainForm : Form
+    {
+        string SocketIP, SocketPort, SocketConnect;
+        string MainBarcode;
+
+        public delegate void callrichtext(String ss);
+        public callrichtext myDelegateStatus;
+        public delegate void callrichtext1(String ss);
+        public callrichtext1 myDelegateReceive;
+
+        private static Socket _ClientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private const int BUFFER_SIZE = 1024;
+        private static readonly byte[] buffer = new byte[BUFFER_SIZE];
+        public MainForm()
+        {
+            InitializeComponent();
+            SocketIP = "10.0.112.165";
+            SocketPort = "9004";
+            TextBox.CheckForIllegalCrossThreadCalls = false;
+            ConnectToSR1000();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            SR1000Scan();
+        }
+
+        private void SR1000Scan()
+        {
+            SendText("LON\r");
+            Thread.Sleep(1000);
+            SendText("LOFF\r");
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SR1000Scan();
+        }
+        private void SendText(string text)
+        {
+            try
+            {
+                byte[] message = Encoding.ASCII.GetBytes(text);
+                _ClientSocket.BeginSend(message, 0, message.Length, SocketFlags.None, new AsyncCallback(SendData), _ClientSocket);
+            }
+            catch (Exception ex)
+            {
+                ResultStatus("Error Msg :: " + ex);
+            }
+        }
+
+        private void ConnectToSR1000()
+        {
+            IPEndPoint iep = new IPEndPoint(IPAddress.Parse(SocketIP), int.Parse(SocketPort));
+            _ClientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            _ClientSocket.Connect(iep);
+            rTBResult.Text = _ClientSocket.Connected.ToString();
+        }
+        private void ReceiveData(IAsyncResult iar)
+        {
+            try
+            {
+                int received;
+                try
+                {
+                    received = _ClientSocket.EndReceive(iar);
+                }
+                catch (SocketException)
+                {
+                    return;
+                }
+
+                if (received == 0) return;
+                var data = new byte[received];
+                Array.Copy(buffer, data, received);
+                string receivedData = Encoding.ASCII.GetString(data);
+
+                ResultStatus(receivedData);
+            }
+            catch (Exception ex)
+            {
+                string vExReceive = "Failed Receive";
+                rTBResult.Invoke(this.myDelegateReceive, new Object[] { vExReceive });
+            }
+        }
+        private void SendData(IAsyncResult iar)
+        {
+            int sent;
+            try
+            {
+                sent = _ClientSocket.EndReceive(iar);
+            }
+            catch (SocketException)
+            {
+                _ClientSocket.Close();
+                return;
+            }
+
+            if (sent == 0) return;
+            try
+            {
+                _ClientSocket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveData), _ClientSocket);
+            }
+            catch (Exception ex)
+            {
+                ResultStatus(ex.Message);
+            }
+        }
+        public void ResultStatus(string RText)
+        {
+            string dateLog = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");
+            rTBResult.AppendText(dateLog + "----> " + RText);
+            rTBResult.AppendText(Environment.NewLine);
+        }
+    }
+}
